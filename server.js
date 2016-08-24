e8d7eb1d886655bbf11c6150aad2bcef3bd08259
Gilbert Kipkorir <kipkorirgilbert@gmail.com>
2016-08-23 21:06:38 -0400

    add server

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-require('express');
+var express = require('express');
+var app = express();
+var port = "8080";
+var host = "0.0.0.0";
+
+app.listen(port, host);
+console.log("Listening on port 8080");
